5fa0ea6bf52f14b2803ca36449d087d163728e8f
lijiyao919 <jiyao.li@aggiemail.usu.edu>
2018-09-29 00:22:03 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from image_loader import *
+import network
+
+training_bee_data, training_no_bee_data, test_bee_data, test_no_bee_data, training_data, test_data = load_data_wrapper()
+
+
+#print training_data
+#print test_data
+net = network.Network([1024, 30, 2])
+net.SGD(training_data, 30, 10, 0.001, training_bee_data=training_bee_data,
+                                     training_no_bee_data=training_no_bee_data,
+                                     test_bee_data=test_bee_data,
+                                     test_no_bee_data=test_no_bee_data,
+                                     test_data=test_data)
